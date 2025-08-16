4213855dc6294f96da75ce996167672f60d8957b
Toheed69 <mughaltoheed29@gmail.com>
2025-08-17 02:26:10 +0500

    Updated Hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import About from "./components/about/About";
+import Portfolio from "./components/portfolio/Portfolio";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <Hero/>
-      <About/>
+
+      <Routes>
+        {/* Home Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <About />
+            </>
+          }
+        />
+
+        {/* Portfolio Page */}
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
     </>
   );
 }
